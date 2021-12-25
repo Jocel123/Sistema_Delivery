307f126d1560cc047b1c2ebe2d537baef968206c
jocel123 <jocelviana@gmail.com>
2021-12-25 19:59:39 -0200

    Banco de dados criado

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, request
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Oi, eu sou o python!')
+    return render(request, 'home.html', {'nome': 'Jocel Viana'})
--- projetodelivery/settings.py
+++ projetodelivery/settings.py
@@ -117,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+SESSION_COOKIE_AGE = 60*60*24*7
+
+SESSION_SAVE_EVERY_REQUEST = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
